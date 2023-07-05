e39398fc5eeea711bc07956cdc464b9d2830929e
fenggnef <lifenggnefil@gmail.com>
2023-07-05 16:15:07 +0800

    Update baud_rate_gen.v
    ACC_MAX should decrease 1 cycle for baudrate clock(both rxclk_en and txclk_en), otherwise RX sampling moment at 8th cycle will run far away from best sampling moment and get a wrong byte, and TX baud width will be 1 cycle wider than common. Especially uart running at a high baudrate such as 115200 or above.

--- baud_rate_gen.v
+++ baud_rate_gen.v
@@ -17,14 +17,14 @@ assign rxclk_en = (rx_acc == 5'd0);
 assign txclk_en = (tx_acc == 9'd0);
 
 always @(posedge clk_50m) begin
-	if (rx_acc == RX_ACC_MAX[RX_ACC_WIDTH - 1:0])
+	if (rx_acc == RX_ACC_MAX[RX_ACC_WIDTH - 1:0]-1)
 		rx_acc <= 0;
 	else
 		rx_acc <= rx_acc + 5'b1;
 end
 
 always @(posedge clk_50m) begin
-	if (tx_acc == TX_ACC_MAX[TX_ACC_WIDTH - 1:0])
+	if (tx_acc == TX_ACC_MAX[TX_ACC_WIDTH - 1:0]-1)
 		tx_acc <= 0;
 	else
 		tx_acc <= tx_acc + 9'b1;
